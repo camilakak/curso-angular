93a3371a3b081acf1c302eb71fc179dc95182580
Camila Katagi <camila.kna@gmail.com>
2018-11-03 15:49:04 -0200

    Criando a aplicação base angular

---
+++ app.js
@@ -0,0 +1,6 @@
+var message = "Hello World";
+console.log(message);
+var episode = 5;
+console.log("This is episode " + 4);
+episode = episode + 1;
+console.log("Next episode is " + episode);
---
+++ app.ts
@@ -0,0 +1,7 @@
+let message: string = "Hello World"
+console.log(message)
+
+let episode: number = 5
+console.log("This is episode " + 4)
+episode = episode + 1
+console.log("Next episode is " + episode);
